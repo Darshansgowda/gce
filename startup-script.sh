3b8df148bf210300bfc525d0c47357eb8c0adf0c
Darshan <darshan.s@prixgen.com>
2020-10-22 12:15:01 +0530

    modified according to my_project need

--- startup-script.sh
+++ startup-script.sh
@@ -30,18 +30,18 @@ useradd -m -d /home/pythonapp pythonapp
 
 # Fetch source code
 export HOME=/root
-git clone https://github.com/GoogleCloudPlatform/getting-started-python.git /opt/app
+git clone https://github.com/Darshansgowda/python-gce-deployment.git /opt/app
 
 # Python environment setup
-virtualenv -p python3 /opt/app/gce/env
-source /opt/app/gce/env/bin/activate
-/opt/app/gce/env/bin/pip install -r /opt/app/gce/requirements.txt
+virtualenv -p python3 /opt/app/python-gce-deployment/env
+source /opt/app/python-gce-deployment/env/bin/activate
+/opt/app/python-gce-deployment/env/bin/pip install -r /opt/app/python-gce-deployment/requirements.txt
 
 # Set ownership to newly created account
 chown -R pythonapp:pythonapp /opt/app
 
 # Put supervisor configuration in proper place
-cp /opt/app/gce/python-app.conf /etc/supervisor/conf.d/python-app.conf
+cp /opt/app/python-gce-deployment/python-app.conf /etc/supervisor/conf.d/python-app.conf
 
 # Start service via supervisorctl
 supervisorctl reread
